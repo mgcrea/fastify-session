216ddbf46f517f50f05a30d82fe66bb8646ee73c
Olivier Louvignes <olivier@mgcrea.io>
2020-11-13 16:27:03 +0100

    feat(update): minor changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fastifyPlugin from 'fastify-plugin';
 import { plugin } from './plugin';
 
 export { FastifySessionOptions } from './plugin';
-export { Session, SessionData } from './session';
+export { Session, SessionData, SessionStore } from './session';
 
 export default fastifyPlugin(plugin, {
   fastify: '3.x',
