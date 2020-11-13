2a82d6aab7fb95113b0ed3a3a32340144454c80b
Olivier Louvignes <olivier@mgcrea.io>
2020-11-13 16:43:48 +0100

    chore(release): cut the 0.9.5 release

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fastifyPlugin from 'fastify-plugin';
 import { plugin } from './plugin';
 
 export { FastifySessionOptions } from './plugin';
-export { Session, SessionData, SessionStore } from './session';
+export { MemoryStore, Session, SessionData, SessionStore } from './session';
 
 export default fastifyPlugin(plugin, {
   fastify: '3.x',
