7e9a69f0a5dbced96318cc8681a6d37fb15fa923
Olivier Louvignes <olivier@mgcrea.io>
2021-01-24 12:34:27 +0100

    feat(update): minor changes

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
-  plugins: ['@typescript-eslint', 'prettier'],
-};
