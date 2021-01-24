9b94632eb3036126b50966d855ea6fcf7892f71a
Olivier Louvignes <olivier@mgcrea.io>
2021-01-24 16:00:49 +0100

    feat(logs): improve logs

--- src/plugin.ts
+++ src/plugin.ts
@@ -59,22 +59,25 @@ export const plugin: FastifyPluginAsync<FastifySessionOptions> = async (fastify,
   // decode/create a session for every request
   fastify.addHook('onRequest', async (request) => {
     const { cookies, log } = request;
+    const bindings = { ...logBindings, hook: 'onRequest' };
 
     const cookie = cookies[cookieName];
     if (!cookie) {
-      log.debug(logBindings, 'There is no cookie, creating an empty session');
       request.session = new Session();
+      log.debug({ ...bindings, sessionId: request.session.id }, 'There was no cookie, created an empty session');
       return;
     }
     try {
-      log.debug(logBindings, 'Found an existing cookie, attempting to decode session');
+      log.debug(bindings, 'Found an existing cookie, attempting to decode session ...');
       request.session = await Session.fromCookie(cookie);
-
-      log.debug(logBindings, 'Session successfully decoded');
+      log.info({ ...bindings, sessionId: request.session.id }, 'Session successfully decoded');
       return;
     } catch (err) {
-      log.debug(logBindings, `Decoding error: ${err.message}, creating an empty session`);
       request.session = new Session();
+      log.warn(
+        { ...bindings, err, sessionId: request.session.id },
+        `Failed to decode existing cookie, created an empty session`
+      );
       return;
     }
   });
@@ -82,31 +85,38 @@ export const plugin: FastifyPluginAsync<FastifySessionOptions> = async (fastify,
   // encode a cookie
   fastify.addHook('onSend', async (request, reply) => {
     const { session, log } = request;
+    const bindings = { ...logBindings, hook: 'onSend' };
 
     if (!session) {
-      log.debug(logBindings, 'There is no session, leaving it as is');
+      log.debug(bindings, 'There was no session, leaving it as is');
       return;
     } else if (!saveUninitialized && !Object.keys(session.data).length) {
-      log.debug(logBindings, "Session is empty and won't be saved, leaving it as is");
+      log.debug(bindings, "Created session is empty and won't be saved, leaving it as is");
       return;
     } else if (!session.changed && !session.created && !session.rotated) {
-      log.debug(logBindings, 'The existing session was not changed, leaving it as is');
+      log.debug({ ...bindings, sessionId: session.id }, 'The existing session was not changed, leaving it as is');
       return;
     } else if (session.deleted) {
-      log.debug(logBindings, 'Deleting session');
       reply.setCookie(cookieName, '', {
         ...cookieOptions,
         ...session[kCookieOptions],
         expires: new Date(0),
         maxAge: 0,
       });
+      log.info({ ...bindings, sessionId: session.id }, 'Deleted session');
       return;
     }
 
-    // code: reply.statusCode
     if (session.created || session.changed) {
-      log.debug('Saving created/changed session');
+      log.debug(
+        { ...bindings, sessionId: session.id },
+        `About to send a ${session.created ? 'created' : 'changed'} session, saving ...`
+      );
       await session.save();
+      log.info(
+        { ...bindings, sessionId: session.id },
+        `${session.created ? 'Created' : 'Changed'} session successfully saved`
+      );
     }
     reply.setCookie(cookieName, await session.toCookie(), { ...cookieOptions, ...session[kCookieOptions] });
   });
