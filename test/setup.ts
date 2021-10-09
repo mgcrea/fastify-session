308ef423ef8bb951c44ae53155dfadaf94e5523c
Olivier Louvignes <olivier@mgcrea.io>
2021-10-09 21:50:50 +0200

    chore(test): fix ts error

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { warn } from 'console';
 import { inspect } from 'util';
-import './typings';
 
-global.d = (...args: unknown[]) =>
+const d = (...args: unknown[]) =>
   setImmediate(() => warn('🔴 ' + inspect(args.length > 1 ? args : args[0], { colors: true, depth: 10 }) + '\n'));
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      d: typeof d;
+    }
+  }
+}
