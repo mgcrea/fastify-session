f60015e4030a5b1e90c112506d2f888d95474988
Olivier Louvignes <olivier@mgcrea.io>
2021-10-14 11:35:14 +0200

    feat(typings): restrict JsonData to JsonObject

--- src/session/SessionData.ts
+++ src/session/SessionData.ts
@@ -11,5 +11,7 @@
  *
  */
 
+import type { JsonObject } from 'type-fest';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface SessionData extends Record<string, unknown> {}
+export interface SessionData extends JsonObject {}
